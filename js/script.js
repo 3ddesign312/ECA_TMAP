7a0a7cc5656407b44ed98533722ab1faf60d9b62
3ddesign312 <eddtheut@gmail.com>
2025-07-10 13:32:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const brandStates = {
   woodmode: ["TN", "KY", "OH", "MI", "IN", "IL", "WI", "MO", "IA", "MN", "ND", "SD", "NE", "WY", "MT", "AZ", "UT", "ID", "NV", "OR", "WA", "CA", "AK", "HI"],
   bridgewood: ["TN", "KY", "OH", "MI", "IN", "IL", "WI", "MO", "IA", "MN", "ND", "SD", "NE", "MT", "AZ", "UT", "ID", "NV", "OR", "WA", "CA", "AK", "HI"],
   stoll: ["AZ", "CA", "CO", "HI", "TN", "ID", "IL", "IN", "IA", "KS", "KY", "MI", "MN", "MT", "MO", "NE", "ND", "SD", "NV", "OH", "OR", "WA", "WI", "WY", "UT"],
-  danver: ["IA", "IL", "IN", "KS", "MO", "ND", "NE", "SD", "WI", "OH"]
+  danver: ["IA", "IL", "IN", "KS", "MO","MN", "ND", "NE", "SD", "WI", "OH"]
 };
 
 function highlightStates(brand) {
